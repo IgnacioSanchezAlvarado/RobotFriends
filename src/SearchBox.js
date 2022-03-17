d3d17f57149c9bcc8dbd08965b51ee037f90b02f
IgnacioSanchezAlvarado <nacho92sa@gmail.com>
2022-03-17 19:07:40 +0100

    first

---
+++ src/SearchBox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SearchBox = ({searchfield, searchChange}) => {
+	return(
+		<div className='pa2'>
+		<input 
+		className='tc ba b--green bg-lightest-blue pa3' 
+		type='search' 
+		placeholder='search robots'
+		onChange={searchChange}
+		/>
+		</div>
+		);
+}
+
+export default SearchBox;
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Nacho Sanchez',
+    username: 'Nacho',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Ana Santacruz',
+    username: 'Ana',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Pedro patines',
+    username: 'Pedro',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Patricia conton',
+    username: 'Patri',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Juanin',
+    username: 'Juanin',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Karinna bom',
+    username: 'kk',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicolas Madrudo',
+    username: 'Nico',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Alfonso sierra',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
